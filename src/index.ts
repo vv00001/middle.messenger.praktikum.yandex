03a848a6231cc50d1cd0d07114cede2656341024
My Name <my-name@chromium.org>
2022-10-26 15:45:20 +0700

    chat component start

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { renderDOM, registerComponent } from "./mypracticum"
 
 import { LoginPage } from "./pages/login/login"
 import { NotFoundPage } from "./pages/notFound/notFound"
+import { Chat } from "./pages/chat/chat"
 
 
 
@@ -11,14 +12,14 @@ import Input from     "./component/input"
 import Button from    "./component/button"
 
 import Title from "./component/title/"
-// console.log(MainInput)
 registerComponent(Input);
 registerComponent(Button);
 registerComponent(MainInput);
 registerComponent(Title);
 
 document.addEventListener("DOMContentLoaded", () => {
-   renderDOM(new LoginPage())
+   // renderDOM(new LoginPage())
   //  renderDOM(new NotFoundPage())
+   renderDOM(new Chat())
  })
  
