2afa1c0015df29245af12ac49533336f99b8b4fa
My Name <my-name@chromium.org>
2022-11-11 00:44:04 +0700

    add Router from practicum

---
+++ add.d.ts
@@ -0,0 +1,8 @@
+import { Block } from "./src/mypracticum/Block"
+declare global {
+   // module "uuid";
+   declare module '*';
+
+   export type Values<T extends Record<string, unknown>> = T[Keys<T>]
+   export type Nullable<T> = T | null
+}
