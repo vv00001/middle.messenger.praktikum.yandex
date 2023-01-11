85e0be8c349dee99b3589ea8d6564b726ce871cd
My Name <my-name@chromium.org>
2023-01-11 11:05:58 +0700

    search test 2

--- api/server.js
+++ api/server.js
@@ -6,10 +6,10 @@ const app = express()
 const { PORT = 3000 } = process.env
 const pathName = path.join(__dirname, "../dist/index.html")
 
-app.use(express.static("./dist"))
+app.use(express.static("dist"))
 
 app.get("/*", (req, res) => {
-  res.sendFile(pathName)
+  res.sendFile("index.html", {root: 'src'})
 })
 
-app.listen(PORT)
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
